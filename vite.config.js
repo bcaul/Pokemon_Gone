1d7795537e0fd5866581a46b2958235692a72754
bcaul <bcaulfield68@gmail.com>
2025-11-14 16:54:10 +0000

    Fix PWA build: increase max file size limit to 3 MB

--- vite.config.js
+++ vite.config.js
@@ -29,7 +29,8 @@ export default defineConfig({
         ]
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        maximumFileSizeToCacheInBytes: 3145728 // 3 MB (increased from default 2 MB)
       }
     })
   ],
